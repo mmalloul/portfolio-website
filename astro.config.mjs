b30fd0df22fe2ee1181a65967a6f6f3aa97222d5
Mohammed Malloul <mohammed.malloul@hotmail.com>
2024-08-07 21:21:59 +0200

    ci: removed server build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 import icon from 'astro-icon';
-import node from '@astrojs/node';
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	output: 'hybrid',
 	integrations: [tailwind(), preact(), icon(), svelte()],
-	adapter: node({
-		mode: 'middleware'
-	})
 });
