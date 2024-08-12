0d2b8091ef3d4914f854e910f8f87b26756f1654
Mohammed Malloul <mohammed.malloul@hotmail.com>
2024-08-12 09:40:38 +0200

    feat: added a services section on the homepage

--- src/lib/data/nav-items.ts
+++ src/lib/data/nav-items.ts
@@ -2,6 +2,7 @@ export const navigationItems = [
 	{ name: 'Home', href: '/' },
 	{ name: 'About', href: '/#about' },
 	{ name: 'Skills', href: '/#skills' },
+	{ name: 'Services', href: '/#services' },
 	{ name: 'Projects', href: '/#projects' },
 	{ name: 'Contact', href: '/#contact' }
 ];
