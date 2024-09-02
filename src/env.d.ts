75a13d52ce2c288afa30c8053e49d4e99c417ef9
Mohammed Malloul <mohammed.malloul@hotmail.com>
2024-09-02 23:20:46 +0200

    feat: replaced project astro content collection with cms data

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+	readonly PUBLIC_CMS_API_URL: string;
+	readonly PUBLIC_MEDIA_BASE_URL: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
