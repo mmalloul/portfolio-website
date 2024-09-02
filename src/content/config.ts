384553f87ba543750dd38a89b2e8052ee89519b7
Mohammed Malloul <mohammed.malloul@hotmail.com>
2024-09-03 01:04:46 +0200

    refactor: cleanup

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,9 @@
 import { aboutCollection } from './about/config';
-import { projectsCollection } from './projects/config';
 import { contactCollection } from './contact/config';
 import { skillsCollection } from './skills/config';
 
 export const collections = {
 	about: aboutCollection,
-	projects: projectsCollection,
 	contact: contactCollection,
 	skills: skillsCollection
 };
