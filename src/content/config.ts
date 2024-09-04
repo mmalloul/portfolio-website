2cfa697330574d100183a90ac2f690cbc8b5c161
Mohammed <94612697+mmalloul@users.noreply.github.com>
2024-09-04 16:13:35 +0200

    refactor: removed and updated old content collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,5 @@
 import { aboutCollection } from './about/config';
-import { contactCollection } from './contact/config';
-import { skillsCollection } from './skills/config';
 
 export const collections = {
-	about: aboutCollection,
-	contact: contactCollection,
-	skills: skillsCollection
+	about: aboutCollection
 };
