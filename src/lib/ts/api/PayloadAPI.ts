c548177bd13cd3519bab4d01f444a134df0703a9
Mohammed <94612697+mmalloul@users.noreply.github.com>
2024-09-04 16:13:59 +0200

    feat: added Payload API cleint

---
+++ src/lib/ts/api/PayloadAPI.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from './httpClient';
+import type { Project } from '@lib/ts/types/project';
+import type { Contact } from '@lib/ts/types/contact';
+import type { Skill } from '@lib/ts/types/skill';
+import type { Service } from '@lib/ts/types/service';
+
+export class PayloadAPI {
+	private httpClient: HttpClient;
+
+	constructor(httpClient: HttpClient) {
+		this.httpClient = httpClient;
+	}
+
+	// Project API calls
+	async getAllProjects(): Promise<Project[]> {
+		return this.httpClient.get<Project[]>('/projects');
+	}
+	async getFeaturedProjects(): Promise<Project[]> {
+		return this.httpClient.get<Project[]>('/projects?where[isFeatured][equals]=true');
+	}
+
+	async getProjectBySlug(slug: string): Promise<Project | null> {
+		const projects = await this.httpClient.get<Project[]>(`/projects?where[slug][equals]=${slug}`);
+		return projects.length ? projects[0] : null;
+	}
+
+	// Contact API calls
+	async getAllContacts(): Promise<Contact[]> {
+		return this.httpClient.get<Contact[]>('/contact-info');
+	}
+
+	async getContactById(id: string): Promise<Contact> {
+		return this.httpClient.get<Contact>(`/contact-info/${id}`);
+	}
+
+	// Skill API calls
+	async getAllSkills(): Promise<Skill[]> {
+		return this.httpClient.get<Skill[]>('/skills');
+	}
+
+	// Service API calls
+	async getAllServices(): Promise<Service[]> {
+		return this.httpClient.get<Service[]>('/services');
+	}
+}
+
+// Instantiate PayloadAPI
+const apiUrl: string = import.meta.env.PUBLIC_CMS_API_URL;
+const mediaBaseUrl: string = import.meta.env.PUBLIC_MEDIA_BASE_URL;
+export const payloadApi = new PayloadAPI(new HttpClient(apiUrl, mediaBaseUrl));
